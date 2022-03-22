209d88c317d96063a32357c8874afcd974b9ee73
djackson29 <danika.c.jackson@gmail.com>
2022-03-21 20:54:21 -0700

    countries API is working

---
+++ components/Card.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  props: {
+    country: Object
+  }
+}
+</script>
+
+<template>
+<div class="card">
+  <!-- <h2 class="card__title">{{ country.title }}</h2>
+  <p class="card__copy">{{ country.genre }}</p>
+  <p class="card__copy">{{ country.plot }}</p>
+  <p class="card__copy">{{ country.rated }}</p> -->
+  <h2 class="card__title">{{ country.name.common }}</h2>
+  <p class="card__copy">{{ country.subregion }}</p>
+
+</div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.card {
+  border: 1px solid black;
+  border-radius: 0;
+  margin: 10px 0;
+  &__title {
+    font-size: 1.5rem;
+  }
+  &__copy {
+
+  }
+}
+
+</style>
--- pages/motw.vue
+++ pages/motw.vue
@@ -1,14 +1,17 @@
 <template>
-<div class="movieWeek">
   <main class="main">
 
     <!-- grid row with one column for title of page -->
     <div class="row">
       <div class="col">
         <h2>Movie of the Week Archives</h2>
-        <!-- <div class="container" v-if="movies">
-          <card v-for="movie of moviesAPI" :key="movie.id" :movie="movie" />
-        </div> -->
+        <div v-if="countries">
+          <card
+            v-for="country of countries"
+            :key="country.id"
+            :country="country"
+          />
+        </div>
       </div>
     </div>
 
@@ -33,54 +36,37 @@
       </div>
     </div> -->
   </main>
-</div>
 </template>
 
 
 
 <script>
-// import axios from 'axios'
-// import Card from '~/components/Card.vue'
+import axios from 'axios'
+import Card from '~/components/Card.vue'
 
 export default {
-  name: 'movieWeek'
-  // components: {
-  //   Card
-  // },
-  // data() {
-  //   return {
-  //     loading: true,
-  //     moviesAPI: null,
-  //     errored: false
-  //     array for placeholder content
-  //     placeholderContent: [
-  //       "<placeholder for API data>",
-  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  //     ]
-  //   }
-  // },
-  // mounted () {
-  // axios
-  //   .get('http://www.omdbapi.com/?i=tt3896198&apikey=d36f6554')
-  //   .then(response => (this.moviesAPI = response.data))
-  //   .catch(error => {
-  //     console.log(error)
-  //     this.errored = true
-  //   })
-  //   .finally(() => this.loading = false)
-  // }
+  name: 'movieWeek',
+  components: {
+    Card
+  },
+  data() {
+    return {
+      loading: true,
+      countries: null,
+      errored: false
+    }
+  },
+  mounted () {
+  axios
+    .get('https://restcountries.com/v3.1/region/europe')
+    .then(response => (this.countries = response.data))
+    .catch(error => {
+      console.log(error)
+      this.errored = true
+    })
+    .finally(() => this.loading = false)
+  }
 }
-
-
-// fetch('http://www.omdbapi.com/?i=tt3896198&apikey=d36f6554')
-//   .then(data =>
-//   data.json()
-// )
-// .then(data => {
-//   console.log(data);
-//   document.querySelector('pre').innerHTML = JSON.stringify(data);
-// })
-// .catch(error => console.error(error));
 
 </script>
 
