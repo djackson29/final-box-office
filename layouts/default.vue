9e9cffed7325b80b599c0afc30c6a383b74b6645
djackson29 <danika.c.jackson@gmail.com>
2022-03-21 18:16:07 -0700

    nuxt project working

---
+++ layouts/default.vue
@@ -0,0 +1,110 @@
+<template>
+<div id="app">
+
+  <!-- use bootsrap grid layout -->
+  <div class="container">
+
+    <!-- main nav, routers to create a navigation between /views files -->
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link> |
+      <nuxt-link to="/comingSoon">Coming Soon</nuxt-link> |
+      <nuxt-link to="/discoverTitles">Discover New Titles</nuxt-link> |
+      <nuxt-link to="/motw">Movie of the Week</nuxt-link>
+    </nav>
+
+    <!-- populate area between main nav and footer with content from /pages files  -->
+    <Nuxt />
+
+    <!-- footer -->
+    <footer>
+
+      <!-- 2 column grid layout for footer links -->
+      <div class="row">
+        <hr>
+        <div class="col-sm">
+
+          <!-- v-for to iterate through quick links object -->
+          <div v-for="quick in quickLinks" v-bind:key="quick.id">
+            <h3>{{ quick.name }}</h3>
+            <ul>
+              <li><a v-bind:href="quick.linku1">{{ quick.link1 }}</a></li>
+              <li><a v-bind:href="quick.linku2">{{ quick.link2 }}</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm">
+
+          <!-- v-for to iterate through social links object -->
+          <div v-for="social in socialLinks" v-bind:key="social.id">
+            <h3>{{ social.name }}</h3>
+            <ul>
+              <li><a v-bind:href="social.linku1" target="_blank">{{ social.link1 }}</a></li>
+              <li><a v-bind:href="social.linku2" target="_blank">{{ social.link2 }}</a></li>
+              <li><a v-bind:href="social.linku3" target="_blank">{{ social.link3 }}</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</div>
+</template>
+
+
+<script>
+
+
+export default {
+  name: 'app',
+  data() {
+    return {
+
+      // objects for quick links and social media links in footer
+      quickLinks: [{
+        name: 'Quick Links',
+        link1: 'Coming Soon',
+        linku1: '/comingSoon',
+        link2: 'Archives',
+        linku2: '/movieWeek'
+      }],
+      socialLinks: [{
+        name: 'Follow Us',
+        link1: 'Facebook',
+        linku1: 'https://www.facebook.com/',
+        link2: 'Instagram',
+        linku2: 'https://www.instagram.com/',
+        link3: 'Twitter',
+        linku3: 'https://twitter.com/'
+      }]
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  padding: 0 10rem;
+}
+
+nav {
+  padding: 30px 0;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #8C4CCD;
+}
+
+footer {
+  padding: 50px 0;
+}
+</style>
---
+++ pages/motw.vue
@@ -0,0 +1,91 @@
+<template>
+<div class="movieWeek">
+  <main class="main">
+
+    <!-- grid row with one column for title of page -->
+    <div class="row">
+      <div class="col">
+        <h2>Movie of the Week Archives</h2>
+        <!-- <div class="container" v-if="movies">
+          <card v-for="movie of moviesAPI" :key="movie.id" :movie="movie" />
+        </div> -->
+      </div>
+    </div>
+
+    <!-- grid row with 3 columns for main content of page -->
+    <!-- <div class="row"> -->
+
+      <!-- v-for to iterate through the placeholder array and populate each column with placeholder content -->
+      <!-- <div class="col-lg">
+        <div v-for="placeholder in placeholderContent" v-bind:key="placeholder.id">
+          <p>{{placeholder}}</p>
+        </div>
+      </div>
+      <div class="col-lg">
+        <div v-for="placeholder in placeholderContent" v-bind:key="placeholder.id">
+          <p>{{placeholder}}</p>
+        </div>
+      </div>
+      <div class="col-lg">
+        <div v-for="placeholder in placeholderContent" v-bind:key="placeholder.id">
+          <p>{{placeholder}}</p>
+        </div>
+      </div>
+    </div> -->
+  </main>
+</div>
+</template>
+
+
+
+<script>
+// import axios from 'axios'
+// import Card from '~/components/Card.vue'
+
+export default {
+  name: 'movieWeek'
+  // components: {
+  //   Card
+  // },
+  // data() {
+  //   return {
+  //     loading: true,
+  //     moviesAPI: null,
+  //     errored: false
+  //     array for placeholder content
+  //     placeholderContent: [
+  //       "<placeholder for API data>",
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  //     ]
+  //   }
+  // },
+  // mounted () {
+  // axios
+  //   .get('http://www.omdbapi.com/?i=tt3896198&apikey=d36f6554')
+  //   .then(response => (this.moviesAPI = response.data))
+  //   .catch(error => {
+  //     console.log(error)
+  //     this.errored = true
+  //   })
+  //   .finally(() => this.loading = false)
+  // }
+}
+
+
+// fetch('http://www.omdbapi.com/?i=tt3896198&apikey=d36f6554')
+//   .then(data =>
+//   data.json()
+// )
+// .then(data => {
+//   console.log(data);
+//   document.querySelector('pre').innerHTML = JSON.stringify(data);
+// })
+// .catch(error => console.error(error));
+
+</script>
+
+
+
+<style lang="scss">
+
+</style>
